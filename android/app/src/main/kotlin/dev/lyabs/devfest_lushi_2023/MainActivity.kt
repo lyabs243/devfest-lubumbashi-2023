ca1bf75cba334f51565b36f85ae5b85044ec1315
Loïc Yabili <loicyabili@outlook.com>
2023-12-06 16:26:04 +0200

    Init source code

---
+++ android/app/src/main/kotlin/dev/lyabs/devfest_lushi_2023/MainActivity.kt
@@ -0,0 +1,6 @@
+package dev.lyabs.devfest_lushi_2023
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,75 @@
+import 'package:devfest_lushi_2023/components/app_button.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.red),
+        useMaterial3: true,
+      ),
+      home: const MyHomePage(title: 'Devfest Lubumbashi 23'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+
+  @override
+  Widget build(BuildContext context) {
+
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: Text(widget.title),
+      ),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(20),
+          child: Column(
+            children: [
+              AppButton(
+                text: 'Custom Painter',
+                onPressed: () {},
+              ),
+              AppButton(
+                text: 'Animations',
+                onPressed: () {},
+              ),
+              AppButton(
+                text: 'Lottie',
+                onPressed: () {},
+              ),
+              AppButton(
+                text: 'Rive',
+                onPressed: () {},
+              ),
+              AppButton(
+                text: 'All in one',
+                onPressed: () {},
+              ),
+            ],
+          ),
+        ),
+      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
